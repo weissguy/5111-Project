c1ef402993123b464dc96547300158700b8cfb19
Eunsu Choi <u7920491@anu.edu.au>
2024-12-09 20:24:01 -0500

    lots of things

--- mol_to_sound/main.py
+++ mol_to_sound/main.py
@@ -1,14 +1,26 @@
 from notjazzy import *
+from miditoaudio import to_audio
 import pretty_midi
 
-mol_name = input("enter molecule name:")
-for atom in create_atomic_map(mol_name):
-    ##map z number to name/letter in a dictionary??
+mol_sound = pretty_midi.PrettyMIDI()
 
-molecule = pretty_midi.PrettyMIDI()
-molecule_instr = pretty_midi.Instrument(program = 4)
-print(pretty_midi.hz_to_note_number(440))
-a_note = pretty_midi.Note(velocity = 10, pitch = int(pretty_midi.hz_to_note_number(440)), start =0, end=1)
-molecule_instr.notes.append(a_note)
-molecule.instruments.append(molecule_instr)
-molecule.write("sonification/mid_files/instr4.mid")
+mol_name = input("enter molecule name:")
+atomic_map = create_atomic_map(mol_name)
+print(atomic_map)
+for atom_dict in atomic_map:
+    # getting all the properties
+    atom_num = atom_dict["z"]
+    vib_freq = 441
+    partial_charge = atom_dict["eeq"]
+    lone_pairs = atom_dict["num_lp"]
+    donor_strength = atom_dict["sdx"] + atom_dict["sdc"]
+    # mapping properties to sound
+    molecule_instr = pretty_midi.Instrument(program = atom_num)
+    atom_note = pretty_midi.Note(velocity = int(donor_strength*15), pitch = int(pretty_midi.hz_to_note_number(vib_freq)), start =0, end=lone_pairs)
+    # adding sound to molecule (lowkey need to fix this)
+    molecule_instr.notes.append(atom_note)
+    mol_sound.instruments.append(molecule_instr)
+print(gibbs(mol_name))
+
+mol_sound.write(f"sonification/mid_files/{mol_name}.mid")
+to_audio("allinone.sf2", f"sonification/mid_files/{mol_name}.mid", "sonification/wav_files")
--- mol_to_sound/notjazzy.py
+++ mol_to_sound/notjazzy.py
@@ -5,14 +5,20 @@ import kallisto
 import pubchempy
 import jazzy
 from jazzy.api import atomic_map_from_smiles
+from jazzy.api import deltag_from_smiles
 
 def create_atomic_map(chem_name):
-    return pubchempy.get_compounds(chem_name, "name")[0].isomeric_smiles
+    smiles = pubchempy.get_compounds(chem_name, "name")[0].isomeric_smiles
+    return atomic_map_from_smiles(smiles)
+
+def gibbs(chem_name):
+    smiles = pubchempy.get_compounds(chem_name, "name")[0].isomeric_smiles
+    return deltag_from_smiles(smiles)
 
 # chem_name = input("Chemical name: ").lower()
 
 # smiles = pubchempy.get_compounds(chem_name, "name")[0].isomeric_smiles
 
-print(atomic_map_from_smiles(smiles))
+# print(atomic_map_from_smiles(smiles))
 # atomic_tuples = atomic_tuples_from_smiles(glucose_smiles)
 # print(atomic_tuples)
